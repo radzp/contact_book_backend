63e0da7c10b6f6361b4e4181186ef1a077ffacdb
radzp <anne.paczesna@gmail.com>
2024-06-17 23:58:03 +0200

    Init

---
+++ src/main/java/com/amw/contact_book_backend/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.amw.contact_book_backend.constant;
+
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+
+
+
+
+}
---
+++ src/main/java/com/amw/contact_book_backend/repo/ContactRepo.java
@@ -0,0 +1,13 @@
+package com.amw.contact_book_backend.repo;
+
+
+import com.amw.contact_book_backend.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
